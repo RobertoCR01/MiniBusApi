3fa3220c0d1fc1b93c028457123531745754fb98
Roberto Diaz Garcia <roberto.diaz@itau.com.pa>
2023-05-15 09:40:08 +1200

    Creacion Objetos miniBus

---
+++ MiniBusApi.Domain/Dto/MiniBusDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MiniBusApi.Domain.Dto
+{
+    public class MiniBusDTO
+    {
+        public int Id { get; set; }
+        public int IdCompany { get; set; }
+        public string Brand { get; set; }
+        public string Tipo { get; set; }
+        public string Capacity { get; set; }
+    }
+}
---
+++ MiniBusApi.Domain/Models/MiniBus.cs
@@ -0,0 +1,17 @@
+﻿namespace MiniBusApi.Domain.Models
+{
+    public class MiniBus
+    {
+        public MiniBus() { }
+        public int Id { get; set; }
+        public int IdCompany { get; set; }
+        public string Brand { get; set; }
+        public string Tipo { get; set; }
+        public int Year { get; set; }
+        public string Capacity { get; set; }
+        public string? UserInsert { get; set; }
+        public DateTime? InsertionDate { get; set; }
+        public string? UserModifies { get; set; }
+        public DateTime? ModificationDate { get; set; }
+    }
+}
