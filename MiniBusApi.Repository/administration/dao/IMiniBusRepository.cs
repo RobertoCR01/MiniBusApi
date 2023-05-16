7077f82c3f78a988c3f2534aaba7d96429c0b996
Roberto Diaz Garcia <roberto.diaz@itau.com.pa>
2023-05-17 11:44:58 +1200

    Repository Dao

---
+++ MiniBusApi.Repository/administration/dao/IMiniBusRepository.cs
@@ -0,0 +1,21 @@
+﻿using MiniBusApi.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace MiniBusApi.Repository.administration.dao
+{
+    public interface IMiniBusRepository : IDisposable
+    {
+        Task<IEnumerable<MiniBus>> GetMinibus();
+        Task<MiniBus> GetMinibusByID(int minibusID);
+        Task<ActionResult<MiniBus>> InsertMinibus(MiniBus minibus);
+        Task<MiniBus> DeleteMinibus(int minibusID);
+        Task<ActionResult<MiniBus>> UpdateMinibus(int minibusID, MiniBus minibus);
+        void Save();
+    }
+}
---
+++ MiniBusApi.Repository/administration/dao/impl/MinibusRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MiniBusApi.Domain.Models;
+using MiniBusApi.Repository.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBusApi.Repository.administration.dao.impl
+{
+    public class MinibusRepository : IMiniBusRepository, IDisposable
+    {
+        private ApplicationDbContext _db;
+        public MinibusRepository(ApplicationDbContext context)
+        {
+            this._db = context;
+        }
+        public async Task<MiniBus> DeleteMinibus(int minibusID)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDisposable.Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<MiniBus>> GetMinibus()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public async Task<MiniBus> GetMinibusByID(int minibusID)
+        {
+            var miniBus = await _db.Minibuses.SingleOrDefaultAsync(m => m.Id == minibusID);
+            return miniBus;
+        }
+
+        public async Task<ActionResult<MiniBus>> InsertMinibus(MiniBus minibus)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IMiniBusRepository.Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ActionResult<MiniBus>> UpdateMinibus(int minibusID, MiniBus minibus)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
