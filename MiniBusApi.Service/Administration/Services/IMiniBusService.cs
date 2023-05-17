73dc8a8376720e319ef576001221ab7dbd6b8d60
Roberto Diaz Garcia <roberto.diaz@itau.com.pa>
2023-05-17 12:10:01 +1200

    Services Configurado

---
+++ MiniBusApi.Service/Administration/Services/IMiniBusService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MiniBusApi.Domain.Models;
+
+namespace MiniBusApi.Service.Administration.Services
+{
+    internal interface IMiniBusService
+    {
+        Task<IEnumerable<MiniBus>> GetMinibus(String loggedUser, DateTime currentDate);
+        Task<ActionResult<MiniBus>> GetMiniBusByID(int minibusID, String loggedUser, DateTime currentDate);
+        Task<MiniBus> InsertMinibus(MiniBus minibus, String loggedUser, DateTime currentDate);
+        Task<ActionResult<MiniBus>> DeleteMinibus(int minibusID, String loggedUser, DateTime currentDate);
+        Task<ActionResult<MiniBus>> UpdateMinibus(int minibusID, MiniBus minibus, String loggedUser, DateTime currentDate);
+        void Save();
+    }
+}
---
+++ MiniBusApi.Service/Administration/Services/Impl/MiniBusService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniBusApi.Domain.Models;
+using MiniBusApi.Repository.administration.dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBusApi.Service.Administration.Services.Impl
+{
+    public class MiniBusService : IMiniBusService
+        {
+        private IMiniBusRepository _miniBusRepository;
+
+        public MiniBusService(IMiniBusRepository miniBusRepository)
+        {
+            this._miniBusRepository = miniBusRepository;
+        }
+        public async Task<ActionResult<MiniBus>> DeleteMinibus(int minibusID, string loggedUser, DateTime currentDate)
+            {
+                throw new NotImplementedException();
+            }
+
+            public async Task<IEnumerable<MiniBus>> GetMinibus(string loggedUser, DateTime currentDate)
+            {
+                throw new NotImplementedException();
+            }
+
+            public async Task<ActionResult<MiniBus>> GetMiniBusByID(int minibusID, string loggedUser, DateTime currentDate)
+            {
+                MiniBus minibus = await _miniBusRepository.GetMinibusByID(minibusID);
+                return minibus;
+            }
+
+            public async Task<MiniBus> InsertMinibus(MiniBus minibusProcesar, string loggedUser, DateTime currentDate)
+            {
+                throw new NotImplementedException();
+            }
+
+            void IMiniBusService.Save()
+            {
+                throw new NotImplementedException();
+            }
+
+            public async Task<ActionResult<MiniBus>> UpdateMinibus(int minibusID, MiniBus minibus, string loggedUser, DateTime currentDate)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
